19f56628b77d02d9f69748c2832476ee035cca85
Laura <lroxana.amaya@gmail.com>
2025-02-06 22:19:18 -0500

    Antes del perfil del estudiante

---
+++ src/app/students-dashboard/home-student/home-student.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-student',
+  imports: [],
+  templateUrl: './home-student.component.html',
+  styleUrl: './home-student.component.scss'
+})
+export class HomeStudentComponent {
+
+}
+
+
