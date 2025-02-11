482c39a670601f823cff6878c5b4460978d40aa1
rebenew <rebenews@gmail.com>
2025-02-11 18:24:29 -0500

    Actualizaciones de menú y componente 'Header'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterModule, SidebarComponent]
+  imports: [RouterModule, SidebarComponent, HeaderComponent]
 })
 export class AppComponent {
   menuAbierto = false;
