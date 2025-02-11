93e59b41e84bb2fb076247fc5cbf14e2e1cc86d7
appitoriadev <contact@appitoria.dev>
2025-02-11 17:54:11 -0500

    Add CLI analytics configuration, update start script to open browser, and enhance app component imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'omega';
+  constructor(private readonly router: Router) { }
 }
