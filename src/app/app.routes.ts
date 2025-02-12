537bfc5717de473ecd321f030683e8c9431beaee
HernanSanchez77 <your-email@example.com>
2025-02-12 18:36:42 -0500

    Configuración de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './Dashboard_Mentor/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'dashboard_mentor', component: DashboardComponent}
+];
