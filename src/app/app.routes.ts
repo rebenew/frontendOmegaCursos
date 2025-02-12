82bc45fe95591e1dc451344d35aac1b95d8a3817
James Orozco <jamesorozcoh@gmail.com>
2025-02-11 19:17:14 -0500

    Add MoreCourses and CoursesDetails components with routing and service integration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { CoursesDetailsComponent } from './students-dashboard/courses-details/courses-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'more-courses',
+    title: 'More Courses',
+    loadComponent: () =>
+      import('./students-dashboard/more-courses/more-courses.component').then(
+        (m) => m.MoreCoursesComponent
+      ),
+  },
+  {
+    path: 'courses-details/:id',
+    title: 'Courses details',
+    loadComponent: () =>
+      import(
+        './students-dashboard/courses-details/courses-details.component'
+      ).then((m) => m.CoursesDetailsComponent),
+  },
+  { path: '', redirectTo: '/more-courses', pathMatch: 'full' },
+];
