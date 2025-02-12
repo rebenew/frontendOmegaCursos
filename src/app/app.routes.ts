d4e1dbae04631d708d35ad16b273ed2e5336200f
ManuelaAriasG <manuelaarias11@gmail.com>
2025-02-11 20:11:33 -0500

    Landing page component done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+     path: "landing-page",
+     component: LandingPageComponent,
+
+    }
+];
