72ff51d4f9662f24a0e3b9a4b704cab3843514c3
HernanSanchez77 <your-email@example.com>
2025-02-17 17:56:33 -0500

    Refactorizando componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
-
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, RouterLink, SidebarComponent, HeaderComponent],
+  imports: [RouterOutlet],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'omega';
-  
-  menuAbierto = false;
-  constructor(private readonly router: Router) { }
-  toggleMenu() {
-    this.menuAbierto = !this.menuAbierto;
-  }
-
 }
