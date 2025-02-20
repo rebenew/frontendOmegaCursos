127f2f3b9ddb7ff9da531476424f13fdedd69cdc
appitoriadev <contact@appitoria.dev>
2025-02-19 22:35:49 -0500

    Refactor routing structure in app.routes.ts and fix image tag issues in home-student.component.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,114 +8,117 @@ import { VistaCursosComponent } from './vista-cursos/vista-cursos.component';
 
 
 export const routes: Routes = [
-  {path: 'admin-dashboard', 
-   component: AdminDashboardComponent,
-   children: [
   {
-   path: 'dashboard',
-   loadComponent: () => import('./components/admin-course-components/admin-dashboard/admin-dashboard.component')
-     .then(m => m.AdminDashboardComponent),
-   canActivate: [AuthGuard] 
- },
- {
-   path: 'courses',
-   loadComponent: () => import('./components/admin-course-components/admin-course-list/admin-course-list.component')
-     .then(m => m.AdminCourseListComponent),
-   canActivate: [AuthGuard] 
- },
- {
-   path: 'courses/new',
-   loadComponent: () => import('./components/admin-course-components/admin-course-form/admin-course-form.component')
-     .then(m => m.AdminCourseFormComponent),
-   canActivate: [AuthGuard] 
- },
- {
-   path: 'courses/edit/:id',
-   loadComponent: () => import('./components/admin-course-components/admin-course-form/admin-course-form.component')
-     .then(m => m.AdminCourseFormComponent),
-   canActivate: [AuthGuard] 
- },
- {
-   path: 'login',
-   loadComponent: () => import('./components/admin-course-components/login/login.component')
-     .then(m => m.LoginComponent)
- },
- 
- {
-  path: '',
-  component: VistaCursosComponent,
-},
+    path: '',
+    component: VistaCursosComponent
+  },
+  //admin-dashboard
+  {
+    path: 'admin-dashboard',
+    component: AdminDashboardComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./components/admin-course-components/admin-dashboard/admin-dashboard.component')
+          .then(m => m.AdminDashboardComponent),
+        // canActivate: [AuthGuard]
+      },
+      {
+        path: 'courses',
+        loadComponent: () => import('./components/admin-course-components/admin-course-list/admin-course-list.component')
+          .then(m => m.AdminCourseListComponent),
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'courses/new',
+        loadComponent: () => import('./components/admin-course-components/admin-course-form/admin-course-form.component')
+          .then(m => m.AdminCourseFormComponent),
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'courses/edit/:id',
+        loadComponent: () => import('./components/admin-course-components/admin-course-form/admin-course-form.component')
+          .then(m => m.AdminCourseFormComponent),
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'login',
+        loadComponent: () => import('./components/admin-course-components/login/login.component')
+          .then(m => m.LoginComponent)
+      },
 
-{ path: 'dashboard_mentor', component: DashboardComponent },
-{ path: 'dashboard', component: AdminDashboardComponent },
-{
-  path: 'home-student',
-  title: 'Home - Student',
-  component: HomeStudentComponent,
-},
+      // {
+      //   path: '',
+      //   component: VistaCursosComponent,
+      // },
+      // { path: 'dashboard', component: AdminDashboardComponent },
+    ],
+  },
+  //Dashboard mentor
+  { path: 'dashboard_mentor', component: DashboardComponent },
+  //Home Student Component
+  {
+    path: 'home-student',
+    title: 'Home - Student',
+    component: HomeStudentComponent,
+  },
+  // More courses
+  {
+    path: 'more-courses',
+    title: 'More Courses',
+    loadComponent: () =>
+      import('./students-dashboard/more-courses/more-courses.component').then(
+        (m) => m.MoreCoursesComponent
+      ),
+  },
 
-// More courses
-{
-  path: 'more-courses',
-  title: 'More Courses',
-  loadComponent: () =>
-    import('./students-dashboard/more-courses/more-courses.component').then(
-      (m) => m.MoreCoursesComponent
-    ),
-},
-
-//More course details
-{
-  path: 'courses-details/:id',
-  title: 'Courses details',
-  loadComponent: () =>
-    import(
-      './students-dashboard/more-courses/courses-details/courses-details.component'
-    ).then((m) => m.CoursesDetailsComponent),
-},
-
-//Course content
-{
-  path: 'course-content/:id',
-  title: 'Content',
-  component: CourseContentComponent,
-  children: [
-    {
-      path: 'content',
-      loadComponent: () =>
-        import(
-          './students-dashboard/course-content/content/content.component'
-        ).then((m) => m.ContentComponent),
-    },
-    {
-      path: 'learning-tools',
-      loadComponent: () =>
-        import(
-          './students-dashboard/course-content/learning-tools/learning-tools.component'
-        ).then((m) => m.LearningToolsComponent),
-    },
-    {
-      path: 'reviews',
-      loadComponent: () =>
-        import(
-          './students-dashboard/course-content/reviews/reviews.component'
-        ).then((m) => m.ReviewsComponent),
-    },
-    {
-      path: 'q&a',
-      loadComponent: () =>
-        import(
-          './students-dashboard/course-content/questions-answers/questions-answers.component'
-        ).then((m) => m.QuestionsAnswersComponent),
-    },
-  ],
-},
-
-//Default path
-{ path: '**', redirectTo: '', pathMatch: 'full' },
-
-   ]
-  }
+  //More course details
+  {
+    path: 'courses-details/:id',
+    title: 'Courses details',
+    loadComponent: () =>
+      import(
+        './students-dashboard/more-courses/courses-details/courses-details.component'
+      ).then((m) => m.CoursesDetailsComponent),
+  },
+  //Course content
+  {
+    path: 'course-content/:id',
+    title: 'Content',
+    component: CourseContentComponent,
+    children: [
+      {
+        path: 'content',
+        loadComponent: () =>
+          import(
+            './students-dashboard/course-content/content/content.component'
+          ).then((m) => m.ContentComponent),
+      },
+      {
+        path: 'learning-tools',
+        loadComponent: () =>
+          import(
+            './students-dashboard/course-content/learning-tools/learning-tools.component'
+          ).then((m) => m.LearningToolsComponent),
+      },
+      {
+        path: 'reviews',
+        loadComponent: () =>
+          import(
+            './students-dashboard/course-content/reviews/reviews.component'
+          ).then((m) => m.ReviewsComponent),
+      },
+      {
+        path: 'q&a',
+        loadComponent: () =>
+          import(
+            './students-dashboard/course-content/questions-answers/questions-answers.component'
+          ).then((m) => m.QuestionsAnswersComponent),
+      },
+    ],
+  },
+  //Default path
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 
