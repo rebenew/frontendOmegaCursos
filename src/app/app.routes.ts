dbf0b44a21cbe243d0008cb2acf9e2ce104a1b04
ManuelaAriasG <manuelaarias11@gmail.com>
2025-02-19 22:29:28 -0500

    signup component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './Pages/landing-page/landing-page.component';
 import { LoginPageComponent } from './Pages/login-page/login-page.component';
+import { CoursesPageComponent } from './Pages/courses-page/courses-page.component';
+import { SignupPageComponent } from './Pages/signup-page/signup-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'landing', pathMatch: 'full' }, // Redirige la raíz a la landing page
   { path: 'landing', component: LandingPageComponent },
-  { path: 'login', component: LoginPageComponent }
+  { path: 'login', component: LoginPageComponent },
+  { path: 'cursos', component: CoursesPageComponent },
+  { path: 'signup', component: SignupPageComponent},
+
 
 ];
