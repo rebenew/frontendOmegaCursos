c3192399ea93709bdea97218e6f46c3d5367a6a0
appitoriadev <contact@appitoria.dev>
2025-02-26 23:23:33 -0500

    Integrate RouterOutlet into VistaCursosComponent for improved routing functionality

--- src/app/vista-cursos/vista-cursos.component.ts
+++ src/app/vista-cursos/vista-cursos.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SideMenuComponent } from '../side-menu/side-menu.component';
 import { FooterMenuComponent } from '../footer-menu/footer-menu.component';
+import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-vista-cursos',
-  imports: [SideMenuComponent, FooterMenuComponent],
+  imports: [SideMenuComponent, FooterMenuComponent, RouterOutlet],
   templateUrl: './vista-cursos.component.html',
   styleUrl: './vista-cursos.component.scss'
 })
