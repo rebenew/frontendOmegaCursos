4c26a3596b7d4f4d7444d0e021d019e5149fae10
ViviSuarez22 <vivisuarez22@hotmail.com>
2025-02-28 17:08:01 -0500

    detalle de usuario

--- src/app/admin-components/user-detail/user-detail.component.ts
+++ src/app/admin-components/user-detail/user-detail.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../services/admin-user-dashboard-services/user.service';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-detail',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.scss'
+  styleUrls: ['./user-detail.component.scss']
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
+  user$: Observable<any> | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.user$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        const id = +params.get('id')!;
+        return this.userService.getUserById(id);
+      })
+    );
+  }
 }
