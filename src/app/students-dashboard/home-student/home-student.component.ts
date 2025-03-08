0df05bfc258dfcc784e83faac13f58a573116a3d
Laura <lroxana.amaya@gmail.com>
2025-03-07 19:13:43 -0500

    Eliminar vite

--- src/app/students-dashboard/home-student/home-student.component.ts
+++ src/app/students-dashboard/home-student/home-student.component.ts
@@ -19,32 +19,32 @@ export class HomeStudentComponent implements OnInit {
   protected visible: boolean = false;
   activateSideContent: boolean = false;
 
-  @ViewChild('contentCards', { static: true })
-  contentCards!: ElementRef<HTMLElement>;
+  // @ViewChild('contentCards', { static: true })
+  // contentCards!: ElementRef<HTMLElement>;
 
   constructor(
     private renderer: Renderer2,
     private SidecontentService: SidecontentService
   ) {}
-  ngOnInit(): void {
-    setTimeout(() => {
-      //console.log(this.contentCards.nativeElement);
-      const cardCourses =
-        this.contentCards.nativeElement.querySelectorAll('.card-courses');
-      //console.log(cardCourses);
+  // ngOnInit(): void {
+  //   setTimeout(() => {
+  //     //console.log(this.contentCards.nativeElement);
+  //     const cardCourses =
+  //       this.contentCards.nativeElement.querySelectorAll('.card-courses');
+  //     //console.log(cardCourses);
 
-      this.SidecontentService.activateSideContent.subscribe((value) => {
-        this.activateSideContent = value;
-        if (this.activateSideContent) {
-          cardCourses?.forEach((element) => {
-            this.renderer.addClass(element, 'openSideContentCourses');
-          });
-        } else {
-          cardCourses?.forEach((element) => {
-            this.renderer.removeClass(element, 'openSideContentCourses');
-          });
-        }
-      });
-    }, 1000);
-  }
+  //     this.SidecontentService.activateSideContent.subscribe((value) => {
+  //       this.activateSideContent = value;
+  //       if (this.activateSideContent) {
+  //         cardCourses?.forEach((element) => {
+  //           this.renderer.addClass(element, 'openSideContentCourses');
+  //         });
+  //       } else {
+  //         cardCourses?.forEach((element) => {
+  //           this.renderer.removeClass(element, 'openSideContentCourses');
+  //         });
+  //       }
+  //     });
+  //   }, 1000);
+  // }
 }
