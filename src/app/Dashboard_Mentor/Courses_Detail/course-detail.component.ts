56780d16568b9746a9a5db92826ed3f3d43a4477
rebenew <rebenews@gmail.com>
2025-03-12 02:41:24 -0500

    feat: Remove unused variable

--- src/app/Dashboard_Mentor/Courses_Detail/course-detail.component.ts
+++ src/app/Dashboard_Mentor/Courses_Detail/course-detail.component.ts
@@ -20,7 +20,7 @@ export class CourseDetailComponent implements OnInit {
   modalOpen: boolean = false;
   ModalOpenNotas: boolean = false;
   selectedStudent: any = null;
-courseDetail: any;
+
 
 
   constructor(
